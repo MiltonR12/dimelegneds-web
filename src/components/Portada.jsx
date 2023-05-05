f15bbbefd2b4993c7a8a5658649b78fe151f6ee2
MiltonR12 <109563498+MiltonR12@users.noreply.github.com>
2023-05-04 22:38:40 -0400

    arreglo de links

--- src/components/Portada.jsx
+++ src/components/Portada.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 function Portada() {
+
+  const navigate = useNavigate()
+
   return (
     <section className='flex flex-col md:text-left justify-center min-h-screen text-center gap-5 px-2 md:px-10 bg-fondo pt-16 bg-cover bg-center bg-no-repeat' >
       <h1 className='text-5xl font-anton tracking-widest md:text-7xl max-w-xl' >
@@ -9,7 +12,9 @@ function Portada() {
       <p className='text-xl text-slate-200 md:text-2xl max-w-xl' >
         ¡Únete a la competición y conviértete en el campeón del juego!
       </p>
-      <button className='text-slate-900 bg-rose-600 text-2xl py-1 w-full max-w-xs
+      <button 
+      onClick={() => {navigate('/torneos')}}
+      className='text-slate-900 bg-rose-600 text-2xl py-1 w-full max-w-xs
       rounded-3xl transition-all hover:text-rose-500 border-rose-600 border-2 hover:bg-black mx-auto md:mx-0 md:text-3xl' >
         PARTICIPAR
       </button>
